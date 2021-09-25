1a401f4e0de537c40e57ac013906cd3dda49ae2c
David Hollis <david@hollis.computer>
2021-09-25 21:14:06 +0000

    Add a method to load a layout value from a starlark expression

--- packages/layout/src/layout.star
+++ packages/layout/src/layout.star
@@ -1,1 +1,65 @@
-Layout = record(elements = list.type)
+# ---------------------
+# -- reference types --
+# ---------------------
+
+def ref(path):
+    return struct(
+        type = "ref",
+        path = path
+    )
+
+
+# -------------------
+# -- card geometry --
+# -------------------
+
+def insets(top, right, bottom, left):
+    return struct(
+        type = "insets",
+        top = top,
+        right = right,
+        bottom = bottom,
+        left = left,
+    )
+
+def uniform(length):
+    return insets(
+        top = length,
+        right = length,
+        bottom = length,
+        left = length,
+    )
+
+def geometry(units, width, height, cut = uniform(0), safe = uniform(0)):
+    return struct(
+        type = "geometry",
+        units = units,
+        width = width,
+        height = height,
+        cut = cut,
+        safe = safe,
+    )
+
+# ------------
+# -- colors --
+# ------------
+
+def solid(color):
+    return struct(
+        type = "fill",
+        filltype = "solid",
+        color = color,
+    )
+
+
+# ----------------------------
+# -- the layout constructor --
+# ----------------------------
+
+def layout(geometry, background, elements = []):
+    return struct(
+        type = "layout",
+        geometry = geometry,
+        background = background,
+        elements = elements,
+    )
--- packages/layout/src/lib.rs
+++ packages/layout/src/lib.rs
@@ -1,11 +1,16 @@
 #[macro_use]
 extern crate lazy_static;
 
+#[macro_use]
+extern crate anyhow;
+
 use starlark::eval::Evaluator;
 use starlark::environment::{ FrozenModule, Globals, Module };
 use starlark::syntax::{ AstModule, Dialect };
+use starlark::values::OwnedFrozenValue;
 
 const LAYOUT_SRC: &str = include_str!("layout.star");
+const LAYOUT_CONFIG_SYMBOL: &str = "#layout_config";
 
 lazy_static! {
     static ref LAYOUT_LIB: FrozenModule = {
@@ -20,11 +25,78 @@ lazy_static! {
     };
 }
 
+pub struct Layout<'l> {
+    pub name: &'l str,
+    layout_config: OwnedFrozenValue,
+}
+
+impl<'l> Layout<'l> {
+    pub fn from_string(layout_name: &'l str, layout_filename: &str, code: &str) -> anyhow::Result<Layout<'l>> {
+        // Execute `code` in a module with layout.star pre-imported
+        let ast = AstModule::parse(layout_filename, code.to_string(), &Dialect::Extended)?;
+        let globals = Globals::extended();
+        let module = Module::new();
+        module.import_public_symbols(&LAYOUT_LIB);
+        // TODO: add support for `load` statements
+        let mut eval = Evaluator::new(&module, &globals);
+        let layout_config_value = eval.eval_module(ast)?;
+
+        // Extract the type attribute from the result
+        let (_, layout_config_type_attr) =
+            layout_config_value
+                .get_attr("type", eval.heap())
+                .expect("Malformed layout: No type attribute.");
+        let layout_config_type =
+            layout_config_type_attr
+                .unpack_str()
+                .expect("Malformed layout: Type attribute is not a string.");
+
+        // Verify that the type is "layout"
+        if layout_config_type == "layout" {
+            // Get a frozen version of the value by putting it into the module with a name that
+            // isn't a valid starlark identifier, freezing the module, then pulling it back out
+            // as an owned frozen value.
+            module.set(LAYOUT_CONFIG_SYMBOL, layout_config_value);
+            let frozen_module = module.freeze()?;
+            match frozen_module.get(LAYOUT_CONFIG_SYMBOL) {
+                Some(layout_config) => {
+                    Ok(Layout {
+                        name: layout_name,
+                        layout_config: layout_config,
+                    })
+                },
+                None => {
+                    Err(anyhow!("Failed to extract layout config from frozen module."))
+                }
+            }
+        } else {
+            Err(anyhow!("Malformed layout: Unexpected type '{}' for layout config. Should have been 'layout'.", layout_config_type))
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_loads_the_layout_module() {
         use super::LAYOUT_LIB;
-        assert!(LAYOUT_LIB.get("Layout").is_some());
+        assert!(LAYOUT_LIB.get("layout").is_some());
+    }
+
+    #[test]
+    fn it_constructs_layouts() {
+        use super::Layout;
+        let code = r##"
+layout(
+    geometry = geometry(
+        width = 100,
+        height = 100,
+        units = "px",
+    ),
+    background = solid(color = ref(path = "blue")),
+    elements = [],
+)
+        "##;
+        Layout::from_string("test-layout", "test.layout", code).unwrap();
     }
 }
