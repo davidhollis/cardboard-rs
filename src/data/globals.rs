9d5b01d42c060d17f498fb3ed6d3315ff80d8567
David Hollis <david@hollis.computer>
2023-08-29 14:05:48 -0700

    Add a few global styles

--- src/data/globals.rs
+++ src/data/globals.rs
@@ -1,14 +1,41 @@
 use std::{sync::OnceLock, collections::HashMap};
 
-use crate::layout::{model::styles::color::Color, model::Layout};
+use crate::layout::{model::styles::{color::{Color, ColorRef}, TextStyle, font::{Font, Weight}, text::Foreground}, model::Layout, templates::TemplateAwareString};
 
 static BUILTIN_LAYOUTS: OnceLock<HashMap<&'static str, Layout>> = OnceLock::new();
+static BUILTIN_STYLES: OnceLock<HashMap<&'static str, Vec<TextStyle>>> = OnceLock::new();
 
 pub fn init_global_data() -> miette::Result<()> {
     // Explicitly ignore double-initialization, as it should be idempotent
     // TODO(#20): Add some builtin layouts
     // https://github.com/davidhollis/cardboard-rs/issues/20
     let _ = BUILTIN_LAYOUTS.set(HashMap::new());
+
+    let mut style_map = HashMap::new();
+    style_map.insert("b", vec![
+        TextStyle::Font(Font {
+            family: None,
+            weight: Some(Weight::Bold),
+            width: None,
+            style: None,
+        }),
+    ]);
+    style_map.insert("i", vec![
+        TextStyle::Font(Font {
+            family: None,
+            weight: None,
+            width: None,
+            style: Some("italic".to_string()),
+        }),
+    ]);
+    for color in ["black", "dark-gray", "gray", "light-gray", "white", "red", "green", "blue", "yellow", "cyan", "magenta"] {
+        style_map.insert(color, vec![
+            TextStyle::Foreground(Foreground {
+                color: ColorRef::Named(TemplateAwareString::RawString(color.replace("-", " "))),
+            }),
+        ]);
+    }
+
     Ok(())
 }
 
@@ -33,4 +60,10 @@ pub fn color_named(name: &str) -> Option<Color> {
         "magenta" => Some(Color::RGBA(0xff, 0x00, 0xff, 0xff)),
         _ => None,
     }
+}
+
+pub fn style_named(name: &str) -> Option<&'static [TextStyle]> {
+    BUILTIN_STYLES.get()
+        .and_then(|styles| styles.get(name))
+        .map(|style| style.as_slice())
 }
